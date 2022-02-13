5d0d07919a2839f0b9e1f0fb04ed9a8d1c5fb005
Mark Ertmer <mark.ertmer@gmail.com>
2022-02-13 11:30:39 -0700

    User Story 4: Model method & test

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -5,4 +5,9 @@ class Professor <ApplicationRecord
 
   has_many :professor_students
   has_many :students, through: :professor_students
+
+  def average_student_age
+    self.students.average(:age).to_f.round(2)
+  end
+
 end
